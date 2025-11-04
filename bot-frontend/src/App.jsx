b3773159fd5d6517efedb371f0155348ceea5155
AymaneSarsri <sarsri.aymane@etu.uae.ac.ma>
2025-11-04 14:26:01 +0100

    first version of chatbot component

---
+++ bot-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import ChatBot from "./components/Chatbot";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Mon Chatbot</h1>
+      <ChatBot />
+    </div>
+  );
+}
