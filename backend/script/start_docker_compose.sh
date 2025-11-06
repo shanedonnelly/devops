d65a3890fc6e50d267bda8e80486c53d4b7f414e
Omaritto <omaribaf@gmail.com>
2025-11-06 15:35:04 +0100

    v1

--- backend/script/start_docker_compose.sh
+++ backend/script/start_docker_compose.sh
@@ -8,7 +8,7 @@ docker compose ps
 echo "==================================="
 echo "Services are running!"
 echo "==================================="
-echo "Builder Service OpenAPI: http://localh    ost/devops/api/builder-service/docs"
+echo "Builder Service OpenAPI: http://localhost/devops/api/builder-service/docs"
 echo "Catalogue Service OpenAPI: http://localhost/devops/api/catalogue-service/docs"
 echo "pgAdmin (Database UI): http://localhost/devops/api/pgadmin (admin@sitebuilder.com / admin)"
 echo "MinIO Console: http://localhost/devops/api/minio (minioadmin / minioadmin)"
