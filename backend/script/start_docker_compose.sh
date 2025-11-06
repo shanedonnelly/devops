bbf0b356fd9e4711dc4d39dcd816015b50883b9c
shane donnelly <shane@donnelly>
2025-11-06 20:01:15 +0100

    some logos, a few front fixes

--- backend/script/start_docker_compose.sh
+++ backend/script/start_docker_compose.sh
@@ -12,8 +12,9 @@ echo "Builder Service OpenAPI: http://localh    ost/devops/api/builder-service/d
 echo "Catalogue Service OpenAPI: http://localhost/devops/api/catalogue-service/docs"
 echo "pgAdmin (Database UI): http://localhost/devops/api/pgadmin (admin@sitebuilder.com / admin)"
 echo "MinIO Console: http://localhost/devops/api/minio (minioadmin / minioadmin)"
+echo " Front end of shanify : http://localhost/devops/shanify"
 echo "==================================="
 
 sleep 6
 
-firefox http://localhost/devops/api/builder-service/docs http://localhost/devops/api/catalogue-service/docs http://localhost/devops/api/pgadmin http://localhost/devops/api/minio 
+firefox http://localhost/devops/api/builder-service/docs http://localhost/devops/api/catalogue-service/docs http://localhost/devops/api/pgadmin http://localhost/devops/api/minio http://localhost/devops/shanify
